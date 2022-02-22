45e47f389da5a5a8c1fd3151b4c9585f896a5d25
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-02-22 18:36:17 +0530

    updated schema

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema({
+    img:{type:String, required:true},
     name:{type:String, required:true},
+    about:{type:String, required:true},
     price:{type:Number, required:true},
-    rating:{type:String, required:true},
-    product_type:{type:String, required:true},
-    brand:{type:String, required:true},
-    img1:{type:String, required:true},
-    img2:{type:String, required:true},
-    img3:{type:String, required:true},
-    img4:{type:String, required:true},
-    description:{type:String, required:true}
+    reviews:{type:String, required:true},
+    star1:{type:Number, required:true},
+    desc:{type:String, required:true},
+    point1:{type:String, required:true},
+    point2:{type:String, required:true},
+    point3:{type:String, required:true},
+    point4:{type:String, required:true},
+    why:{type:String, required:true},
+    what:{type:String, required:true},
+    how:{type:String, required:true},
+    sideimg1:{type:String, required:true},
+    sideimg2:{type:String, required:true},
+    img22:{type:String, required:true},
+    youtube:{type:String, required:true},
+    star:{type:String, required:true},
 },{
 
 versionKey:false,
