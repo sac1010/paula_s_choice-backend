ae92b4113308be04442a8e642016554f76e6c8c1
RajarsiMukherjee <96103218+RajarsiMukherjee@users.noreply.github.com>
2022-02-23 22:03:31 +0530

    Update dnav.js
    updated

--- public/scripts/dnav.js
+++ public/scripts/dnav.js
@@ -25,7 +25,7 @@ function navbar(){
         </button>
     </div>
     <div class="dpaula">
-        <a href="./index.html">
+        <a href="./index">
         <p id="goupside">PAULA'S CHOICE</p>
         <h3>SKINCARE</h3>
     </a>
@@ -72,7 +72,7 @@ function navbar(){
         </div>
     </div>
     <div class="dbag">
-        <a href="cart.html"><i id="twentyeight" class="fas fa-shopping-bag"></i></a>
+        <a href="/cart"><i id="twentyeight" class="fas fa-shopping-bag"></i></a>
         <h3 id="dbaganu">BAG</h3>
         <p id="dcount">0</p>
     </div>
@@ -88,40 +88,40 @@ function navbar(){
         <div class="dbox">
             <div class="dcnt">
                 <div class="dhvr">
-                <a href="product_data.html"><p>Body Care</p></a>
-                <a href="product_data.html"><p>Cleanser</p></a>
-                <a href="product_data.html"><p>Exfoliant</p></a>
+                <a href="/product_data"><p>Body Care</p></a>
+                <a href="/product_data"><p>Cleanser</p></a>
+                <a href="/product_data"><p>Exfoliant</p></a>
                     <div class="dm dhvr">
-                    <a href="product_data.html"><p>AHA Exfoliant</p></a>
-                    <a href="product_data.html"><p>BHA Exfoliant</p></a>
+                    <a href="/product_data"><p>AHA Exfoliant</p></a>
+                    <a href="/product_data"><p>BHA Exfoliant</p></a>
                     </div>
                 </div>
                 <div class="dhvr">
-                <a href="product_data.html"><p>Eye Cream</p></a>
-                <a href="product_data.html"><p>Face Masks</p></a>
-                <a href="product_data.html"><p>Face Serum</p></a>
-                <a href="product_data.html"><p>Face Toner</p></a>
-                <a href="product_data.html"><p>Hair Care</p></a>
+                <a href="/product_data"><p>Eye Cream</p></a>
+                <a href="/product_data"><p>Face Masks</p></a>
+                <a href="/product_data"><p>Face Serum</p></a>
+                <a href="/product_data"><p>Face Toner</p></a>
+                <a href="/product_data"><p>Hair Care</p></a>
                 </div>
                 <div class="dhvr">
-                <a href="product_data.html"><p>Lip Care</p></a>
-                <a href="product_data.html"><p>Makeup</p></a>
-                <a href="product_data.html"><p>Men's Skin Care</p></a>
+                <a href="/product_data"><p>Lip Care</p></a>
+                <a href="/product_data"><p>Makeup</p></a>
+                <a href="/product_data"><p>Men's Skin Care</p></a>
                 </div>
                 <div class="dhvr">
                     <p>Moisturizer</p>
                     <div class="dm dhvr">
-                    <a href="product_data.html"><p>Dry Skin Moisturizer</p></a>
-                    <a href="product_data.html"><p>Anti-Aging Moisturizer</p></a>
-                    <a href="product_data.html"><p>Oily Skin Moisturizer</p></a>
+                    <a href="/product_data"><p>Dry Skin Moisturizer</p></a>
+                    <a href="/product_data"><p>Anti-Aging Moisturizer</p></a>
+                    <a href="/product_data"><p>Oily Skin Moisturizer</p></a>
                     </div>
                     <p>Natural Skin Care</p>
                 </div>
                 <div class="dhvr">
-                <a href="product_data.html"><p>Skin Booster</p></a>
-                <a href="product_data.html"><p>Skin Brigthener</p></a>
-                <a href="product_data.html"><p>Skin Supplements</p></a>
-                <a href="product_data.html"><p>Sunscreen</p></a>
+                <a href="/product_data"><p>Skin Booster</p></a>
+                <a href="/product_data"><p>Skin Brigthener</p></a>
+                <a href="/product_data"><p>Skin Supplements</p></a>
+                <a href="/product_data"><p>Sunscreen</p></a>
                 </div>
             </div>
             <div class="dicns">
@@ -134,26 +134,26 @@ function navbar(){
         <p>SKIN CONCERNS</p>
         <div class="dbox dbox1">
             <div class="dhvr">
-            <a href="product_data.html"><p>Acne-Prone</p></a>
+            <a href="/product_data"><p>Acne-Prone</p></a>
             <div class="dm dhvr">
-            <a href="product_data.html"><p>Benzoyl Peroxide Treatments</p></a>
+            <a href="/product_data"><p>Benzoyl Peroxide Treatments</p></a>
             </div>
-            <a href="product_data.html"><p>Anti-Aging</p></a>
+            <a href="/product_data"><p>Anti-Aging</p></a>
             <div class="dm dhvr">
-            <a href="product_data.html"><p>Wrinkles</p></a>
+            <a href="/product_data"><p>Wrinkles</p></a>
             </div>
-            <a href="product_data.html"><p>Anti-Pollution Skin Care</p></a>
-            <a href="product_data.html"><p>Blackheads</p></a>
-            <a href="product_data.html"><p>Bumpy Skin</p></a>
+            <a href="/product_data"><p>Anti-Pollution Skin Care</p></a>
+            <a href="/product_data"><p>Blackheads</p></a>
+            <a href="/product_data"><p>Bumpy Skin</p></a>
             </div>
             <div class="dhvr">
-            <a href="product_data.html"><p>Dehydrated Skin</p></a>
-                <a href="product_data.html"><p>Dull Skin</p></a>
-                <a href="product_data.html"><p>Clogged Pores</p></a>
-                <a href="product_data.html"><p>Oil Control</p></a>
-                <a href="product_data.html"><p>Skin Firming</p></a>
-                <a href="product_data.html"><p>Skin Redness</p></a>
-                <a href="product_data.html"><p>Uneven Skin Tone</p></a>
+            <a href="/product_data"><p>Dehydrated Skin</p></a>
+                <a href="/product_data"><p>Dull Skin</p></a>
+                <a href="/product_data"><p>Clogged Pores</p></a>
+                <a href="/product_data"><p>Oil Control</p></a>
+                <a href="/product_data"><p>Skin Firming</p></a>
+                <a href="/product_data"><p>Skin Redness</p></a>
+                <a href="/product_data"><p>Uneven Skin Tone</p></a>
             </div>
         </div>
     </div>
@@ -162,32 +162,32 @@ function navbar(){
         <div class="dbox dbox2">
             <div class="dd">
                 <div class="dhvr">
-                <a href="product_data.html"><p>Dry Skin</p></a>
+                <a href="/product_data"><p>Dry Skin</p></a>
                     <div class="dm dhvr">
-                    <a href="product_data.html"><p>Face Wash for Dry Skin</p></a>
-                    <a href="product_data.html"><p>Moisturizer for Dry Skin</p></a>
-                    <a href="product_data.html"><p>Toner for Dry Skin</p></a>
+                    <a href="/product_data"><p>Face Wash for Dry Skin</p></a>
+                    <a href="/product_data"><p>Moisturizer for Dry Skin</p></a>
+                    <a href="/product_data"><p>Toner for Dry Skin</p></a>
                     </div>
-                    <a href="product_data.html"><p>Normal Skin</p></a>
-                    <a href="product_data.html"><p>Combination Skill</p></a>
+                    <a href="/product_data"><p>Normal Skin</p></a>
+                    <a href="/product_data"><p>Combination Skill</p></a>
                 </div>
                 <div class="dhvr">
-                <a href="product_data.html"><p>Oily Skin</p></a>
+                <a href="/product_data"><p>Oily Skin</p></a>
                     <div class="dm dhvr">
-                    <a href="product_data.html"><p>Face Wash for Oily Skin</p></a>
-                    <a href="product_data.html"><p>Moisturizer for Oily Skin</p></a>
-                    <a href="product_data.html"><p>Toner for Oily Skin</p></a>
+                    <a href="/product_data"><p>Face Wash for Oily Skin</p></a>
+                    <a href="/product_data"><p>Moisturizer for Oily Skin</p></a>
+                    <a href="/product_data"><p>Toner for Oily Skin</p></a>
                     </div>
-                    <a href="product_data.html"><p>Sensitive Skin</p></a>
+                    <a href="/product_data"><p>Sensitive Skin</p></a>
                     <div class="dm dhvr">
-                    <a href="product_data.html"><p>Face Wash for Sensitive Skin</p></a>
-                    <a href="product_data.html"><p>Moisturizer for Sensitive Skin</p></a>
-                    <a href="product_data.html"><p>Toner for Sensitive Skin</p></a>
+                    <a href="/product_data"><p>Face Wash for Sensitive Skin</p></a>
+                    <a href="/product_data"><p>Moisturizer for Sensitive Skin</p></a>
+                    <a href="/product_data"><p>Toner for Sensitive Skin</p></a>
                     </div>
                 </div>
             </div>
             <div class="dd2">
-               <a href="product_data.html"><p>SHOP BY SKIN TYPES ></p></a>
+               <a href="/product_data"><p>SHOP BY SKIN TYPES ></p></a>
             </div>
         </div>
     </div>
@@ -195,20 +195,20 @@ function navbar(){
         <p>INGREDIENTS</p>
         <div class="dbox dbox3">
             <div class="dhvr">
-            <a href="product_data.html"><p>Azelaic Acid</p></a>
-            <a href="product_data.html"><p>Bakuchiol</p></a>
-            <a href="product_data.html"><p>Ceramides</p></a>
-            <a href="product_data.html"><p>Glycolic Acid</p></a>
-            <a href="product_data.html"><p>Hyaluronic Acid</p></a>
-            <a href="product_data.html"><p>Lactic Acid</p></a>
+            <a href="/product_data"><p>Azelaic Acid</p></a>
+            <a href="/product_data"><p>Bakuchiol</p></a>
+            <a href="/product_data"><p>Ceramides</p></a>
+            <a href="/product_data"><p>Glycolic Acid</p></a>
+            <a href="/product_data"><p>Hyaluronic Acid</p></a>
+            <a href="/product_data"><p>Lactic Acid</p></a>
             </div>
             <div class="dhvr">
-            <a href="product_data.html"><p>Niacinamide</p></a>
-            <a href="product_data.html"><p>Omega Fatty Acids</p></a>
-            <a href="product_data.html"><p>Peptides</p></a>
-            <a href="product_data.html"><p>Retinol</p></a>
-            <a href="product_data.html"><p>Salicylic Acid</p></a>
-            <a href="product_data.html"><p>Vitamin C</p></a>
+            <a href="/product_data"><p>Niacinamide</p></a>
+            <a href="/product_data"><p>Omega Fatty Acids</p></a>
+            <a href="/product_data"><p>Peptides</p></a>
+            <a href="/product_data"><p>Retinol</p></a>
+            <a href="/product_data"><p>Salicylic Acid</p></a>
+            <a href="/product_data"><p>Vitamin C</p></a>
             </div>
         </div>
     </div>
@@ -216,11 +216,11 @@ function navbar(){
         <p>TRENDING</p>
         <div class="dbox dbox4">
             <div class="dhvr">
-            <a href="product_data.html"><p>Award Winners</p></a>
-            <a href="product_data.html"><p>Collections</p></a>
-            <a href="product_data.html"><p>Expert Picks</p></a>
-            <a href="product_data.html"><p>Offers</p></a>
-            <a href="product_data.html"><p>Top Rated</p></a>
+            <a href="/product_data"><p>Award Winners</p></a>
+            <a href="/product_data"><p>Collections</p></a>
+            <a href="/product_data"><p>Expert Picks</p></a>
+            <a href="/product_data"><p>Offers</p></a>
+            <a href="/product_data"><p>Top Rated</p></a>
             </div>
             <div>
                 <img src="https://www.paulaschoice.com/on/demandware.static/-/Library-Sites-paulachoice/default/dw70632ec7/images/plp-card/pc_210516_desktop_plp-card_glamour-beauty-award.jpg" alt="">
@@ -231,14 +231,14 @@ function navbar(){
         <p>SKIN CARE RESOURCES</p>
         <div class="dbox dbox5">
             <div class="dhvr">
-            <a href="product_data.html"><p>Routine Finder</p></a>
-            <a href="product_data.html"><p>Skin Quiz</p></a>
-            <a href="product_data.html"><p>Expert Advice</p></a>
+            <a href="/product_data"><p>Routine Finder</p></a>
+            <a href="/product_data"><p>Skin Quiz</p></a>
+            <a href="/product_data"><p>Expert Advice</p></a>
                 <div class="dm dhvr">
-                <a href="product_data.html"><p>Ingredient Dictionary</p></a>
-                <a href="product_data.html"><p>Skin Care Advice</p></a>
+                <a href="/product_data"><p>Ingredient Dictionary</p></a>
+                <a href="/product_data"><p>Skin Care Advice</p></a>
                 </div>
-                <a href="product_data.html"><p>Paula's Choice Perks</p></a>
+                <a href="/product_data"><p>Paula's Choice Perks</p></a>
             </div>
             <div>
                 <img src="https://www.paulaschoice.com/on/demandware.static/-/Library-Sites-paulachoice/default/dwb588c324/images/plp-card/pc_210303_desktop_plp-card_loyalty-sub_cta.jpg" alt="">
