a69c4ff60d736e1d96c315460972938a43d20513
Dhiraj-Giri <96107673+Dhiraj-Giri@users.noreply.github.com>
2022-02-23 23:14:06 +0530

    commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
     email : {type:String, required:false, unique:true},
@@ -11,8 +12,19 @@ timestamps:true
 
 })
 
+UserSchema.pre("save",function(next){
+    if(! this.isModified("password")) return next()
 
-UserSchema.methods.verifyPassword 
+
+    let hash = bcrypt.hashSync(this.password,8)
+    this.password = hash
+    return next()
+})
+
+
+UserSchema.methods.verifyPassword = function(password){
+    return bcrypt.compareSync(password,this.password)
+}
 
 
 
