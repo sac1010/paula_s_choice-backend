4e3c203f0e8e673ea32cbcefcaaad8086cbb27e3
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-02-23 15:12:14 +0530

    add to cart fixed
    checks whether a product is present in the cart and update its quantity

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,12 +1,37 @@
-const express = require("express")
+const express = require("express");
 const Cart = require("../models/cart.model")
 const router = express.Router()
 
 router.post("/", async(req, res)=>{
+    let userId = req.body.userId
     try{
-       const cart = await Cart.create(req.body)
-       res.status(200).send(cart)
-    }catch(e){
+    let cart = await Cart.findOne({userId}).lean().exec() 
+        //checks if cart already exists for the user
+    if(cart){
+        let products = cart.products
+        //check if the product already exists in the cart if then increse the quantity
+        for(let i=0; i<products.length; i++){
+            if(products[i].productId==req.body.products[0].productId){
+               products[i].quantity++
+               cart = await Cart.findOneAndUpdate({userId}, {products}, {new:true}).lean().exec()
+               console.log(cart)
+               return res.send(cart)
+            }
+        }
+        //product not present in cart, hence pushing the product and updating in database
+        products.push(req.body.products[0])
+        cart = await Cart.findOneAndUpdate({userId}, {products}, {new:true}).lean().exec()
+        console.log(cart)
+        return res.send(cart)
+
+    }
+    else{
+        //cart doesnt exist for user, hence crating a new cart
+        const newCart = await Cart.create(req.body)
+        res.status(201).send(newCart)
+    }
+    }
+     catch(e){
         res.status(500).send(e.message)
     }
 
