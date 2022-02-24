01b216e45299d9cf7d1d27652d5f4d4baa1f48c8
RajarsiMukherjee <96103218+RajarsiMukherjee@users.noreply.github.com>
2022-02-24 14:06:41 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cartController= require("./controllers/cart.controller")
 const productsController = require("./controllers/products.controller")
 const commentController = require("./controllers/comment.controller")
 const {register,login} = require("../src/controllers/auth.controller")
+const paymentController = require("./controllers/payment.controller");
 const app = express()
 
 app.use(express.json())
@@ -12,7 +13,7 @@ app.use("/cart", cartController)
 app.use("/comment", commentController)
 app.post("/register",register)
 app.post("/login",login)
-
+app.use("/payment", paymentController);
 
 app.listen(2345, async()=>{
     try{
