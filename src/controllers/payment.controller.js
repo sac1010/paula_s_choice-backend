feeac0b78f4104acdc7714fc8f6f6be740f9ac27
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-02-27 14:24:15 +0530

    payment contrller fix

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -5,18 +5,10 @@ const router = express.Router();
 router.post("/", async(req, res)=>{
   userId = req.body.userId
   try{
-<<<<<<< HEAD
-    const items =await Products.deleteMany(req.body._id)
-    return res.render("ejs/Complete");
-  }
-  catch(err){
-    return res.status(400).send(items)
-=======
     let cart = await Cart.findOneAndDelete({userId}).lean().exec() 
     res.status(200).send("deleted cart")
   }catch(e){
     res.status(500).send(e.message)
->>>>>>> 6bc1153ced196f065eb6247ed7d6a9f04e914a18
   }
  
   
